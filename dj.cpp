3428e7ea653ff6b44164c6c028df4981234cc84c
Sanjana Barman <142417943+SanjanaBarman@users.noreply.github.com>
2024-05-17 23:36:45 +0530

    Add files via upload

---
+++ dj.cpp
@@ -0,0 +1,105 @@
+#include<stdio.h>
+#define INF 999
+int w[100][100];
+int n;
+
+void readGraph(){
+	FILE *fp;
+	fp=fopen("Graph_Dijkstra.txt","r");
+	if(fp==NULL) {
+		printf("File open failed \n");
+		return;
+	}
+	fscanf (fp,"%d",&n);
+	for(int i=0;i<n;i++){
+		for (int j=0;j<n;j++){
+			fscanf (fp,"%d",&w[i][j]);
+		}
+	}
+	fclose(fp);
+}
+
+void showGraph(){
+	for(int i=0; i<n; i++){
+		for(int j=0;j<n;j++){
+			printf("%6d",w[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int parent [100];
+int mstset [100];
+int key [100];
+
+int minKey(){
+	int min =INF;
+	int index =-1;
+	for (int i=0;i<n;i++) {
+		if (mstset[i]==0) {
+			if(key[i]<min){
+				min = key[i];
+				index=i;}
+		}
+	}
+	return index;
+}
+
+void showPath(int v){
+	if (parent[v]==-1){
+		printf ("%3c",v+'A');
+	return; }
+
+	showPath (parent[v]);
+	printf("%3c",v+'A');
+}
+
+void showPathCost (int v){
+
+	printf ("\n Path cost = %d\n", key[v]);
+	return;
+}
+
+
+void Dijkstra(int v)
+{
+	for(int i =0; i<n;i++)
+	{
+		parent[i]=-1 ;
+		mstset[i]=0;
+		key[i]=INF;
+	}
+	key[v]=0;
+	for (int i= 0;i <n;i++) 
+	{
+		int u = minKey();
+		mstset[u]=1;
+		for(v=0; v<n; v++)
+		{
+			if(w[u][v]!=0 && w[u][v]!=INF)
+			{
+				if(mstset[v]==0)
+				{
+					if(key[v]>w[u][v] + key[u])
+					{
+						parent[v] =u;
+						key[v]=w[u][v]+key[u]; 
+					}
+				}
+			}
+		}
+	}
+}
+
+int main() 
+{
+	readGraph();
+	showGraph();
+	Dijkstra(0);
+	for(int i=1;i<n;i++){
+		showPath (i);
+		showPathCost(i);
+	}
+	return 0;
+}
+
