285cf00ecc2ebbca8daf12a2b02e489b8cb6cbc7
Sanjana Barman <142417943+SanjanaBarman@users.noreply.github.com>
2024-05-18 02:04:56 +0530

    Update floydWarshall.c

--- floydWarshall.c
+++ floydWarshall.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define INF 999
-int v;
+int v,count=1;
 
 void printSolution(int D[][v], int P[][v])
 {
     int i, j;
-    printf("Shortest distance between all pair of nodes \n");
+    printf("Step %d Shortest distance between all pair of nodes \n",count);
     for(i=0; i<v; i++)
     {
         for(j=0;j<v;j++) 
@@ -15,7 +15,8 @@ void printSolution(int D[][v], int P[][v])
         }
         printf("\n");
     }
-    printf("Predecessor Matrix for shortest path \n");
+    printf("\n");
+   /* printf("Predecessor Matrix for shortest path \n");
     for(i=0; i<v; i++) 
     {
         for(j=0; j<v;j++) 
@@ -23,7 +24,8 @@ void printSolution(int D[][v], int P[][v])
             printf("%d\t", P[i][j]);
         }
         printf ("\n");
-    }
+    }*/
+    count++;
 }
 
 void floydWarshall(int w[][v],int v) 
@@ -47,7 +49,7 @@ void floydWarshall(int w[][v],int v)
                 if(abs(D[i][j]) > abs(D[i][k] + D[k][j]))
                 {
                     D[i][j] = D[i][k] + D[k][j];
-                    P[i][j] = P[k][j];
+                    P[i][j] = k+1;
                 }
             }
         }
@@ -57,21 +59,25 @@ void floydWarshall(int w[][v],int v)
 
 int main() 
 { 
-    FILE *fp = fopen("adjacency1.txt", "r");
+    FILE *fp = fopen("op.txt", "r");
     int i,j;
     if (fp == NULL)
     {
         printf("Error in opening file!!!"); 
         exit(0);
     }
+    
     fscanf(fp,"%d", &v);
+    printf("%d\n",v);
     int G[v][v];
     for(i=0; i<v; i++)
     {
         for(j=0;j<v;j++)
         {
-            if(fscanf(fp, "%d", &G[i][j] != 1))
+            if((fscanf(fp,"%d",&G[i][j])!=1))
+            
             {
+            	
                 return 1;
             }
         }
@@ -88,3 +94,35 @@ int main()
     floydWarshall(G,v);
     return 0;
 }
+
+/*4
+0       999     -2      999
+4       0       3       999
+999     999     0       2
+999     -1      999     0
+Step 1 Shortest distance between all pair of nodes
+0       999     -2      999
+4       0       2       999
+999     999     0       2
+999     -1      997     0
+
+Step 2 Shortest distance between all pair of nodes
+0       999     -2      999
+4       0       2       999
+999     999     0       2
+3       -1      1       0
+
+Step 3 Shortest distance between all pair of nodes
+0       997     -2      0
+4       0       2       4
+999     999     0       2
+3       -1      1       0
+
+Step 4 Shortest distance between all pair of nodes
+0       -1      1       0
+4       0       2       4
+5       1       0       2
+3       -1      1       0
+
+
+--------------------------------*/
