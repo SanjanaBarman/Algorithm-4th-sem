a79232fd6a1fbf46d53ec918d8a4fe6f96d4c562
Sanjana Barman <142417943+SanjanaBarman@users.noreply.github.com>
2024-05-23 01:10:47 +0530

    Update nQueen.c

--- nQueen.c
+++ nQueen.c
@@ -1,73 +1,90 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<math.h>
- 
-int board[20],count;
- 
-int main()
-{
-int n,i,j;
-void queen(int row,int n);
- 
-printf(" - N Queens Problem Using Backtracking -");
-printf("\n\nEnter number of Queens:");
-scanf("%d",&n);
-queen(1,n);
-return 0;
-}
- 
-//function for printing the solution
+int x[20],count=1;
+
 void print(int n)
 {
-int i,j;
-printf("\n\nSolution %d:\n\n",++count);
- 
-for(i=1;i<=n;++i)
-  printf("\t%d",i);
- 
-for(i=1;i<=n;++i)
+	int i,j;
+	printf("\n\nSolution %d:\n\n",count++);
+	for(i=1;i<=n;i++)
+	{
+		printf("\t%d",i);
+	}
+	for(i=1;i<=n;i++)
+	{
+		printf("\n\n%d",i);
+		for(j=1;j<=n;j++)
+		{
+			if(x[i]==j)
+				printf("\tQ");
+			else
+				printf("\t-");
+		}
+	}
+}
+
+int place(int k,int i)
 {
-  printf("\n\n%d",i);
-  for(j=1;j<=n;++j) //for nxn board
-  {
-   if(board[i]==j)
-    printf("\tQ"); //queen at i,j position
-   else
-    printf("\t-"); //empty slot
-  }
+	int j;
+	for(j=1;j<=k-1;j++)
+	{
+		if(x[j]==i)
+			return 0;
+		else
+			if(abs(j-k)==abs(x[j]-i))
+				return 0;
+	}
+	return 1;
 }
-}
- 
-/*funtion to check conflicts
-If no conflict for desired postion returns 1 otherwise returns 0*/
-int place(int row,int column)
+
+void queen(int k,int n)
 {
-int i;
-for(i=1;i<=row-1;++i)
+	int i;
+	for(i=1;i<=n;i++)
+	{
+		if(place(k,i))
+		{
+			x[k]=i;
+			
+			if(k==n)
+				print(n);
+			else
+				queen(k+1,n);
+		}
+	}	
+}
+
+int main()
 {
-  //checking column and digonal conflicts
-  if(board[i]==column)
-   return 0;
-  else
-   if(abs(board[i]-column)==abs(i-row))
-    return 0;
+	int n;
+	printf("The no. of queen:");
+	scanf("%d",&n);
+	queen(1,n);
+	return 0;
 }
- 
-return 1; //no conflicts
-}
- 
-//function to check for proper positioning of queen
-void queen(int row,int n)
-{
-int column;
-for(column=1;column<=n;++column)
-{
-  if(place(row,column))
-  {
-   board[row]=column; //no conflicts so place queen
-   if(row==n) //dead end
-    print(n); //printing the board configuration
-   else //try queen with next position
-    queen(row+1,n);
-  }
-}
-}
+/*The no. of queen:4
+
+
+Solution 1:
+
+        1       2       3       4
+
+1       -       Q       -       -
+
+2       -       -       -       Q
+
+3       Q       -       -       -
+
+4       -       -       Q       -
+
+Solution 2:
+
+        1       2       3       4
+
+1       -       -       Q       -
+
+2       Q       -       -       -
+
+3       -       -       -       Q
+
+4       -       Q       -       -*/
